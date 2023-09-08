0cf90f961e2ab93f66c9daacedfc163d5c8d0afb
tejaromalius <tayefi.ilia@protonmail.com>
2023-09-08 09:02:49 +0300

    Updated Pyproject & Tests
    * Update the test corresponding to changed to Binypt
    * Configure pyproject.toml for build

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,5 +2,5 @@ import binypt
 import pytest
 
 def test_main_ability():
-    binypt_obj = binypt.Binypt("BTCUSDT", "3d", "01/01/2022-00:00:00", "01/01/2023-00:00:00", "test.csv", "resources/.metadata.json")
+    binypt_obj = binypt.Binypt("BTCUSDT", "3d", "01/01/2022-00:00:00", "01/01/2023-00:00:00", "test.csv")
     assert type(binypt_obj) == binypt.Binypt
