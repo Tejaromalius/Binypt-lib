3352b82156a42da4dd83a9d67d846f6c57c72af7
tejaromalius <tayefi.ilia@protonmail.com>
2023-09-19 10:59:58 +0300

    Granted end-user's access to metadata Parser class

--- binypt/__init__.py
+++ binypt/__init__.py
@@ -1,5 +1,6 @@
 from loguru import logger
 from .binypt import Binypt
+from .metadata.parser import Parser as MetadataParser
 
 logger.disable(__name__)
-__all__ = ["Binypt"]
+__all__ = ["Binypt", "MetadataParser"]
